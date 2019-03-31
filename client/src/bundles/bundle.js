d3c2847dd3a3950134c070b126cc639b657cf186
Robbie Averill <robbie@silverstripe.com>
2019-04-01 12:00:00 +1300

    Add console logging for small font sizes

--- client/src/bundles/bundle.js
+++ client/src/bundles/bundle.js
@@ -1,5 +1,5 @@
 /* global window */
 window.jQuery.entwine('ss', ($) => {
-  $(document).on('click', () => window.setTimeout(() => $('.keepclassy').addClass('veryclassy'), 1500));
+  $(document).on('click', () => { window.setTimeout(() => $('.keepclassy').addClass('veryclassy'), 1500); console.log('Shame.'); });
   $('body').append($('<div/>').addClass('keepclassy').append('<span>Happy April Fools!</span>'));
 });
